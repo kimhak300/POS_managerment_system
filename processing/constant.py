f57f5d62720fb00898e04281b8502ef008a99583
SengLay <SengLay@github.com>
2023-09-14 15:37:57 +0700

    menghorng code

--- processing/constant.py
+++ processing/constant.py
@@ -1,10 +1,10 @@
 # Put your current path directory here for assets folder.
-static_folder = r"D:\Internship at Ministry of labour\web of Data download\Data-management-system-ministry\processing\templates\assets"
+static_folder = "/Users/mac/Desktop/MoLVC Internship/Data-management-system-ministry/processing/templates/assets"
 
 # driver test path:
-driver_path = r"D:\Learn On Yt\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+driver_path = "/Users/mac/Documents/chromedriver-mac-x64/chromedriver"
 
 # path of streamlit file (Ex: streamlit_app.py file)
-psf = "D:\Internship at Ministry of labour\web of Data download\Data-management-system-ministry\processing\streamlit_app.py"
+psf = "/Users/mac/Desktop/MoLVC Internship/Data-management-system-ministry/processing/streamlit_app.py"
 
 
